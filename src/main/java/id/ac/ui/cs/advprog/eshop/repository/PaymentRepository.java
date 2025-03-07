8282eb80719fcd0d8e57218eff7e3671ad5e472c
caqqmarina <chiara.aqmarina@ui.ac.id>
2025-03-07 13:06:49 +0700

    [REFACTOR] Optimize PaymentRepository implementation using HashMap for O(1) access.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -4,36 +4,24 @@ import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 @Repository
 public class PaymentRepository {
 
-    private List<Payment> paymentData = new ArrayList<>();
+    private final Map<String, Payment> paymentMap = new HashMap<>();
 
     public Payment save(Payment payment) {
-        int i = 0;
-        for (Payment savedPayment : paymentData) {
-            if (savedPayment.getId().equals(payment.getId())) {
-                paymentData.remove(i);
-                paymentData.add(i, payment);
-                return payment;
-            }
-            i += 1;
-        }
-        paymentData.add(payment);
+        paymentMap.put(payment.getId(), payment);
         return payment;
     }
 
     public Payment findById(String id) {
-        for (Payment savedPayment : paymentData) {
-            if (savedPayment.getId().equals(id)) {
-                return savedPayment;
-            }
-        }
-        return null;
+        return paymentMap.get(id);
     }
 
     public List<Payment> findAll() {
-        return new ArrayList<>(paymentData);
+        return new ArrayList<>(paymentMap.values());
     }
 }
